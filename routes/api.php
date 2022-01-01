f308f3186568986582b2f3be8a46f2945812e024
muhammadmasad <muhammadmasad@folio3.com>
2022-01-01 22:25:17 +0500

    shopify configs setup

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\HomeController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['verify.shopify'])->group(function () {
+    Route::post('/auth', [HomeController::class,'createAuth']);
 });
+
+
